beca44f171891bc659e8a1448d03387fe0fbac2b
Hristiyan Bonev <hristiyanb@aiidatapro.com>
2019-01-23 17:38:26 +0200

    Update item_list.html - add pagination; update sign_up.html

--- shop_project/views.py
+++ shop_project/views.py
@@ -1,6 +1,5 @@
 from .forms import UserCreationForm, UserAuthenticationForm
 from .models import Item, CustomUser
-from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 from django.views.generic import ListView, TemplateView, FormView
@@ -8,16 +7,10 @@ from django.shortcuts import get_object_or_404, redirect, reverse, render
 
 
 class ItemListView(ListView):
-
     model = Item
     template_name = 'item_list.html'
-
-    def get_context_data(self, **kwargs):
-        product_list = Item.objects.all()
-        context = super().get_context_data( **kwargs)
-        context['products'] = product_list
-        return context
-
+    paginate_by = 7
+    context_object_name = 'products'
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -45,7 +38,7 @@ class SignUpView(FormView):
 @login_required
 def add_to_cart(request, **kwargs):
     import ipdb; ipdb.set_trace()
-    user_profile = get_object_or_404(CustomUser, user=request.user)
+    user_profile = get_object_or_404(CustomUser, username=request.user.username)
 
     item = Item.objects.filter(id=kwargs.get('pk', None)).first()
     return redirect(reverse('item_list'))
